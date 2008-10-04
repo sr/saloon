1136223574f593e75e59a2e8b71a14f1b825e384
Simon Rozet <simon@rozet.name>
2008-10-04 23:11:35 +0200

    test cosmetics

--- test/store_test.rb
+++ test/store_test.rb
@@ -6,8 +6,6 @@ describe 'Store' do
     @database = stub('couchdb database', :view => '')
     @store = Store.new(TestDatabase)
     @store.stubs(:database).returns(@database)
-
-    Hash.stubs(:to_atom_entry).returns('im an atom entry!')
   end
 
   it 'has a  db_name' do
@@ -20,13 +18,14 @@ describe 'Store' do
     end
 
     setup do
-      @rows = [{'title' => 'foo', 'content' => 'bar'}]
+      @rows = [{'title' => 'foo', 'subtitle' => 'bar'}]
       @database.stubs(:view).returns('rows' => @rows)
+      Hash.any_instance.stubs(:to_atom_feed).returns(Atom::Feed.new)
     end
 
     it 'finds the collection using the view collection/all' do
-      @database.expects(:view).with('collection/all', :key => 'my_collection', :count => 1).
-        returns('rows' => @rows)
+      @database.expects(:view).with('collection/all',
+        :key => 'my_collection', :count => 1).returns('rows' => @rows)
       do_find
     end
 
@@ -35,13 +34,9 @@ describe 'Store' do
       lambda { do_find }.should.raise CollectionNotFound
     end
 
-    it 'converts the result to an Atom::Feed using Hash#to_atom_feed' do
-      @rows.first.expects(:to_atom_feed)
-      do_find
-    end
-
-    it 'returns an Atom::Feed' do
-      do_find.should.be.an.instance_of(Atom::Feed)
+    it 'coerce the document to an Atom::Feed using #to_atom_feed' do
+      @rows.first.expects(:to_atom_feed).returns('an atom feed')
+      do_find.should.equal 'an atom feed'
     end
   end
 
@@ -58,7 +53,8 @@ describe 'Store' do
 
     it 'finds the entry using the view entry/by_collection' do
       @database.expects(:view).with('entry/by_collection',
-        :key => ['my_collection', 'my_entry'], :count => 1).returns('rows' => @rows)
+        :key => ['my_collection', 'my_entry'], :count => 1).
+        returns('rows' => @rows)
       do_find
     end
 
@@ -67,9 +63,9 @@ describe 'Store' do
       lambda { do_find }.should.raise EntryNotFound
     end
 
-    it 'returns an Atom::Entry' do
-      @rows.first.expects(:to_atom_entry).returns(Atom::Entry.new)
-      do_find.should.be.an.instance_of(Atom::Entry)
+    it 'coerce the document to an Atom::Entry using #to_atom_entry' do
+      @rows.first.expects(:to_atom_entry).returns('an atom entry')
+      do_find.should.equal 'an atom entry'
     end
   end
 end
