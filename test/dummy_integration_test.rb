fa5c523e0a87592ea4cef9a631ba4fc614332e94
Simon Rozet <simon@rozet.name>
2008-10-29 22:18:24 +0100

    improve integration test

--- test/dummy_integration_test.rb
+++ test/dummy_integration_test.rb
@@ -1,8 +1,12 @@
-service = Atom::Service.new('http://0.0.0.0:1234/')
-collection = service.workspaces.first.collections.first
-collection.feed.update!
-count = collection.feed.entries.length
-entry = Atom::Entry.new(:title => 'foo', :content => 'bar')
-collection.post!(entry)
-collection.feed.update!
-raise unless collection.feed.entries.length == count+1
+begin
+  service = Atom::Service.new('http://0.0.0.0:1234/')
+  collection = service.workspaces.first.collections.first
+  collection.feed.update!
+  count = collection.feed.entries.length
+  entry = Atom::Entry.new(:title => 'foo', :content => 'bar')
+  collection.post!(entry)
+  collection.feed.update!
+  collection.feed.entries.length.should.equal count+1
+rescue Errno::ECONNREFUSED
+  puts '#  Please run `ruby lib/app.rb -p1234` to run this test.'
+end
